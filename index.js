db632ec889662a8785e30830a8e0184749a6e7b3
Juha Lindstedt <juha@pakastin.fi>
2024-12-01 17:47:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
+// parse data
 const data = (await Deno.readTextFile("data.txt")).split("\n").map((line) => {
   return line.split("   ");
 });
 
+// split to two arrays
 const arrayA = data.map(([a, b]) => a);
 const arrayB = data.map(([a, b]) => b);
 
+// sort both arrays
 arrayA.sort();
 arrayB.sort();
 
+// calculate distances
 const distances = [];
-
 for (let i = 0; i < arrayA.length; i++) {
   const a = arrayA[i];
   const b = arrayB[i];
@@ -17,9 +20,11 @@ for (let i = 0; i < arrayA.length; i++) {
   distances.push(Math.abs(a - b));
 }
 
+// calculate sum of distances
 const sum = distances.reduce((sum, num) => {
   sum += num;
   return sum;
 }, 0);
 
+// print out result
 console.log(sum);
