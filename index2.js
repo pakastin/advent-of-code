74a4917ef5f190bf30031d17215c0b7fba9f62ea
Juha Lindstedt <juha@pakastin.fi>
2024-12-01 17:48:48 +0200

    Update index2.js

--- index2.js
+++ index2.js
@@ -1,29 +1,36 @@
+// parse data
 const data = (await Deno.readTextFile("data.txt")).split("\n").map((line) => {
   return line.split("   ");
 });
 
+// split to two arrays
 const arrayA = data.map(([a, b]) => a);
 const arrayB = data.map(([a, b]) => b);
 
+// sort both arrays
 arrayA.sort();
 arrayB.sort();
 
-const similarities = [];
+// calculate lookup table for how many same numbers in array B
 const lookup = arrayB.reduce((lookup, item) => {
   lookup[item] || (lookup[item] = 0);
   lookup[item]++;
   return lookup;
 }, {});
 
+// make array of similarities
+const similarities = [];
 for (let i = 0; i < arrayA.length; i++) {
   const a = arrayA[i];
 
   similarities.push(a * lookup[a] || 0);
 }
 
+// sum up
 const sum = similarities.reduce((sum, num) => {
   sum += num;
   return sum;
 }, 0);
 
+// print out result
 console.log(sum);
