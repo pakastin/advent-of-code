48e1b3ffb51c71c7a544c8aa118a4f9d51c69d33
Juha Lindstedt <juha@pakastin.fi>
2024-12-06 17:53:46 +0200

    Refactor from recursion to loop

--- day-6/part-2.js
+++ day-6/part-2.js
@@ -45,8 +45,7 @@ for (let y = 0; y < map.length; y++) {
     if (line[x] === "." && line2[x] === "X") {
       const newMap = map.map((line) => line.slice()).slice();
       newMap[y][x] = "#";
-      console.log(x, y);
-      if (await detectLoop(newMap, hero)) {
+      if (await detectLoop(newMap, { ...hero })) {
         loops++;
       }
     }
@@ -56,44 +55,35 @@ for (let y = 0; y < map.length; y++) {
 console.log(loops);
 
 async function detectLoop(map, hero, visited = {}, count = 0) {
-  if (count % 2000 === 0) {
-    await new Promise((resolve) => setTimeout(resolve, 0));
+  while (true) {
+    const { x, y, dir } = hero;
+    const [dx, dy] = dirs[dir];
+    const nextCell = map[hero.y + dy]?.[hero.x + dx];
+
+    const hasVisited = visited[y]?.[x]?.[dir];
+
+    if (hasVisited) {
+      // if already visited position facing same dir, return true
+      return true;
+    }
+
+    // mark down visited position and dir
+    visited[y] || (visited[y] = {});
+    visited[y][x] || (visited[y][x] = []);
+    visited[y][x][dir] = true;
+
+    // if next cell outside the map, stop
+    if (!nextCell) {
+      return false;
+    }
+
+    // if next cell is obstructed, turn right
+    if (nextCell === "#") {
+      hero.dir = (dir + 1) % 4;
+    } else {
+      // else, walk one step
+      hero.x = x + dx;
+      hero.y = y + dy;
+    }
   }
-  const { x, y, dir } = hero;
-  const [dx, dy] = dirs[dir];
-  const nextCell = map[hero.y + dy]?.[hero.x + dx];
-
-  const hasVisited = visited[y]?.[x]?.[dir];
-
-  if (hasVisited) {
-    // if already visited position facing same dir, return true
-    return true;
-  }
-
-  // mark down visited position and dir
-  visited[y] || (visited[y] = {});
-  visited[y][x] || (visited[y][x] = []);
-  visited[y][x][dir] = true;
-
-  // if next cell outside the map, stop
-  if (!nextCell) {
-    return false;
-  }
-
-  const newHero = {
-    x,
-    y,
-    dir,
-  };
-
-  // if next cell is obstructed, turn right
-  if (nextCell === "#") {
-    newHero.dir = (dir + 1) % 4;
-  } else {
-    // else, walk one step
-    newHero.x = x + dx;
-    newHero.y = y + dy;
-  }
-  // next step
-  return await detectLoop(map, newHero, visited, count + 1);
 }
