aad19488dd41314644f94e7d1f8c02ea5e086358
Juha Lindstedt <juha@pakastin.fi>
2024-12-07 10:36:49 +0200

    D7P1

---
+++ day-7/part-1.js
@@ -0,0 +1,31 @@
+// read map
+const data = (await Deno.readTextFile("./data.txt")).split("\n");
+
+let sum = 0;
+
+for (const line of data) {
+  const targetResult = Number(line.split(":")[0]);
+  const numbers = line
+    .split(" ")
+    .slice(1)
+    .map((str) => Number(str));
+
+  line: for (let i = 0; i < Math.pow(2, numbers.length - 1); i++) {
+    const operations = i.toString(2).padStart(numbers.length - 1, "0");
+
+    let result = numbers[0];
+    for (let i = 0; i < numbers.length - 1; i++) {
+      if (operations[i] === "1") {
+        result += numbers[i + 1];
+      } else {
+        result *= numbers[i + 1];
+      }
+    }
+    if (result === targetResult) {
+      sum += result;
+      break line;
+    }
+  }
+}
+
+console.log(sum);
