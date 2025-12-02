0ff9fbc7aa95c43cfdebdd548ed8b5bdb0540ce2
Juha Lindstedt <juha@pakastin.fi>
2025-12-02 08:41:46 +0200

    Simplify a bit

--- 2025/02/part-2.js
+++ 2025/02/part-2.js
@@ -12,31 +12,32 @@ for (const range of data.split(",")) {
     if (str.length === 1) {
       continue;
     }
-    let validId = false;
-    for (let i = 1; i <= str.length / 2; i++) {
-      const isFactor = str.length % i === 0;
-      if (!isFactor) {
-        continue;
-      }
-      const factor = i;
-      const found = (() => {
-        for (let i = 1; i < str.length / factor; i++) {
-          const start = i * factor;
-          const end = (i + 1) * factor;
-          const prevStart = (i - 1) * factor;
-          const prevEnd = i * factor;
-          const slice = str.slice(start, end);
-          const prevSlice = str.slice(prevStart, prevEnd);
-          if (slice !== prevSlice) {
-            return false;
-          }
+    const validId = (() => {
+      for (let i = 1; i <= str.length / 2; i++) {
+        const isFactor = str.length % i === 0;
+        if (!isFactor) {
+          continue;
         }
-        return true;
-      })();
-      if (found) {
-        validId = true;
+        const factor = i;
+        const found = (() => {
+          for (let i = 1; i < str.length / factor; i++) {
+            const start = i * factor;
+            const end = (i + 1) * factor;
+            const prevStart = (i - 1) * factor;
+            const prevEnd = i * factor;
+            const slice = str.slice(start, end);
+            const prevSlice = str.slice(prevStart, prevEnd);
+            if (slice !== prevSlice) {
+              return false;
+            }
+          }
+          return true;
+        })();
+        if (found) {
+          return true;
+        }
       }
-    }
+    })();
     if (validId) {
       sum += id;
     }
