7f690dbd12b567ad5b6ad37e6df55676fffc9385
Juha Lindstedt <juha@pakastin.fi>
2025-12-03 12:16:17 +0200

    d3p2

---
+++ 2025/03/part-2.js
@@ -0,0 +1,29 @@
+import { readFile } from "node:fs/promises";
+
+const data = await readFile("data.txt", "utf8");
+
+let sum = 0;
+
+for (let line of data.split("\n")) {
+  if (!line) {
+    continue;
+  }
+  const bank = line.split("").map((str) => Number(str));
+  for (let i = 0; i < bank.length - 1; i++) {
+    const value = bank[i];
+    const nextValue = bank[i + 1];
+
+    if (bank.length > 12) {
+      if (nextValue > value) {
+        bank.splice(i, 1);
+        i -= 2;
+      }
+    }
+  }
+  while (bank.length > 12) {
+    bank.pop();
+  }
+  const value = Number(bank.join(""));
+  sum += value;
+}
+console.log(sum);
