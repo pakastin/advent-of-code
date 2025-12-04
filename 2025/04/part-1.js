d66351b5c8c06850ee5908e5d39fed4bde9f0028
Juha Lindstedt <juha@pakastin.fi>
2025-12-04 09:50:54 +0200

    d4p1

---
+++ 2025/04/part-1.js
@@ -0,0 +1,50 @@
+import { readFile } from "node:fs/promises";
+
+const data = await readFile("data.txt", "utf8");
+const lines = data.split("\n").filter((str) => str);
+
+const grid = new Map();
+const resultGrid = new Array(lines.length);
+
+let result = 0;
+
+for (let y = 0; y < lines.length; y++) {
+  resultGrid[y] = [];
+  for (let x = 0; x < lines[y].length; x++) {
+    const char = lines[y][x];
+    grid.get(y) ?? grid.set(y, new Map());
+    grid.get(y).set(x, char);
+    resultGrid[y][x] = char;
+  }
+}
+
+const dirs = [
+  [-1, -1],
+  [-1, 0],
+  [-1, 1],
+  [0, -1],
+  [0, 1],
+  [1, -1],
+  [1, 0],
+  [1, 1],
+];
+
+for (const y of grid.keys()) {
+  const row = grid.get(y);
+  for (const x of row.keys()) {
+    const char = row.get(x);
+    const neighbors = dirs
+      .map(([dx, dy]) => {
+        return grid.get(y + dy)?.get(x + dx);
+      })
+      .filter((_) => _);
+    const rolls = neighbors.filter((char) => char === "@").length;
+    if (char === "@" && rolls < 4) {
+      resultGrid[y][x] = "x";
+      result++;
+    }
+  }
+}
+
+console.log(resultGrid.map((line) => line.join("")).join("\n"));
+console.log(result);
